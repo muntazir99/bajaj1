c84304a488c2d47a9d9f256164fe8d6046e228a7
Muntazir Jahangir <muntazir9934@gmail.com>
2024-08-02 21:20:28 +0530

    final

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       JSON.parse(jsonInput);
 
       // Make API call
-      const res = await fetch('http://127.0.0.1:8000/api/data', {
+      const res = await fetch('http://127.0.0.1:8000/bfhl/data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
